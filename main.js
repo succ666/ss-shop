e048a05c746310e8c677c64eb4c6b897cdbcf8d6
succ666 <2014649323@qq.com>
2022-06-22 23:58:21 +0800

    首页开发完成

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 import App from './App'
 
+//导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+//请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 请求拦截器
+$http.beforeRequest = function(option){
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+// 响应拦截器
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+
+uni.$showMsg = function(title="数据加载失败！", duration = 1300){
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
